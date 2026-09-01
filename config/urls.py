f7d4ff16f1945a114732afd266c1a91633fc5dda
irincovictor-cmd <irinco.victor@cec.edu.ph>
2026-09-01 16:50:54 +0800

    Fix main URL routing to include api app correctly

--- config/urls.py
+++ config/urls.py
@@ -14,13 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# myapp/urls.py
 from django.contrib import admin
-from django.urls import path, include  # Make sure 'include' is imported
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('api.urls')),
-    # path('home/', include('home.urls')),  # Forwards incoming requests to api/urls.py
+    path('', include('api.urls')),  # Root routes to api app (home at /, about at /about/)
 ]
-
