19400d9b5f7634bf56f49f1e26a218c85b44e892
irincovictor-cmd <irinco.victor@cec.edu.ph>
2026-09-01 16:51:43 +0800

    Add /home/ route so both / and /home/ work

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home/', views.home, name='home_alt'),  # also support /home/
     path('about/', views.about, name='about'),
 ]
