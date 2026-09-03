03edc745f9161a36a8965f6415a01dd3dbecc845
irincovictor-cmd <irinco.victor@cec.edu.ph>
2026-09-03 16:50:27 +0800

    Register Topic in Django admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Topic
 
-# Register your models here.
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('title', 'tag', 'created_at')
+    search_fields = ('title', 'tag', 'summary')
