fdb434f4a497b5f8037bdb0e08e8acdeac083ad2
irincovictor-cmd <irinco.victor@cec.edu.ph>
2026-09-10 16:46:57 +0800

    Register Contact in admin

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Topic
+from .models import Topic, Contact
 
 
 @admin.register(Topic)
@@ -7,3 +7,10 @@ class TopicAdmin(admin.ModelAdmin):
     list_display = ('title', 'tag', 'resource_url', 'created_at')
     search_fields = ('title', 'tag', 'summary')
     fields = ('title', 'tag', 'summary', 'body', 'resource_url', 'resource_label')
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'contact', 'created_at')
+    search_fields = ('name', 'email', 'contact')
+    readonly_fields = ('created_at',)
