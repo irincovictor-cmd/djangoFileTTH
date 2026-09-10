6a72ad8788edcfb311aa4e33392053e830833736
irincovictor-cmd <irinco.victor@cec.edu.ph>
2026-09-10 16:46:44 +0800

    Migration: create Contact table

---
+++ api/migrations/0004_contact.py
@@ -0,0 +1,24 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0003_topic_resource_links_all'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Contact',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100)),
+                ('email', models.EmailField(max_length=254)),
+                ('contact', models.CharField(help_text='Phone or mobile number', max_length=30, verbose_name='Contact number')),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+            ],
+            options={
+                'ordering': ['-created_at'],
+            },
+        ),
+    ]
