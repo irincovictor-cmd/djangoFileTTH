20ed138082ecf0d082fae69a17ec66eca2bec7d7
irincovictor-cmd <irinco.victor@cec.edu.ph>
2026-09-10 17:06:19 +0800

    Add contact_success URL

--- api/urls.py
+++ api/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
 
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('contact/success/', views.contact_success, name='contact_success'),
 ]
